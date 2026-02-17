087f38d7d907d7153586d8d69fbea8e8ec8f28c0
lastminutetech321 <131929417+lastminutetech321@users.noreply.github.com>
2026-02-17 16:42:47 -0500

    Refactor FastAPI routes and add intake form model

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
+class IntakeForm(BaseModel):
+  name:str
+  email:str
+  service_requested: str
+  note:str | None = None
+
+@app.get("/") 
 def root():
-  return {"message": "Nautical Compass is live"}
+    return {"message": "Nautical Compass is live"}
 
 @app.get("/intake")
-def intake():
-  return {"message": "Intake system active"}
+def intake_info():
+    return {"message": "Submit intake via Post request to /intake"}
+
+  @app.post("/intake")
+  def submit_intake(form: IntakeForm):
+      return {
+          "status": "Intake received",
+          "data": form
+    }
+
 @app.get("/favicon.ico", include_in_schema=False)
 def favicon():
-  return FileResponse("favicon.png")
+    return FileResponse("favicon.png")
