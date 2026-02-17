38374ddfb4e64a7ad377ee691910520bcc8e083f
lastminutetech321 <131929417+lastminutetech321@users.noreply.github.com>
2026-02-17 15:36:13 -0500

    Add favicon endpoint to serve favicon image

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ def root():
 @app.get("/intake")
 def intake():
   return {"message": "Intake system active"}
+@app.get("/favicon.ico", include_in_schema=False)
+def favicon():
+  return FileResponse("favicon.png")
