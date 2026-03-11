6abd05c73a0140399b8b3999eb530164d155e627
lastminutetech321 <131929417+lastminutetech321@users.noreply.github.com>
2026-03-11 18:45:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,11 @@ def lead(request: Request):
 
 
 @app.post("/lead")
-def lead_submit(name: str = Form(...), email: str = Form(...), message: str = Form(...)):
+def lead_submit(
+    name: str = Form(...),
+    email: str = Form(...),
+    message: str = Form(...)
+):
     return RedirectResponse("/lead/thanks", status_code=303)
 
 
